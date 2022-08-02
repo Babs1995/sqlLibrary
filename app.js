08a085fb2129e91dd6c8ca18e917c0f5583a29a7
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-08-01 18:26:24 -0700

    changes

---
+++ app.js
@@ -0,0 +1,94 @@
+const Sequelize = require('sequelize');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var books = require('./routes/books');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+//Setting up the middleware view engine to "pug"
+app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+
+//using a static route AND express.static method to serve static public files
+app.use("/static", express.static("public"));
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/', books);
+
+
+// 404 handler
+app.use((req, res) => {
+  const err = new Error();
+  err.status = 404;
+  err.message = "So sorry, this page does not exist!";
+  res.render('page-not-found', {err});
+
+})
+
+
+
+//  Global Error Handler
+app.use((err, req, res, next) => {
+  if (err.status === 404){
+    res.render('page-not-found', { err })
+
+  }else{
+    err.message = "There was a server error!";
+    res.status(err.status || 500);
+    res.render('error', {err});
+  }
+  console.log(`You have hit a ${err.status} error!`);
+ });
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'library.db',
+  logging: false
+
+});
+
+
+// async IIFE
+(async () => {
+  // Sync all tables
+
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ force: true });
+
+    console.log("Connection to the database successful!");
+
+  } catch (error) {
+    if (error.name === "SequelizeValidationError") {
+      const errors = error.errors.map((err) => err.message);
+      console.error("Validation errors: ", errors);
+  } else {
+    throw error;
+  }
+}
+
+})();
+
+
+
+
+
+module.exports = app;
---
+++ routes/books.js
@@ -0,0 +1,160 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../models").Book;
+const limit = 5;
+const { Op } = require("sequelize");
+
+/* Async handler from Treehouse. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+router.get(
+  "/books",
+  asyncHandler(async (req, res) => {
+    const allBooks = await Book.findAll({
+      order: [["createdAt", "DESC"]],
+    });
+
+    const pages = Math.ceil(allBooks.length / limit);
+    let page = 1;
+    if (req.query.page) {
+      page = req.query.page;
+    }
+    let search = false;
+    const books = allBooks.slice((page - 1) * limit, limit * page);
+    res.render("index", { books, pages, page });
+  })
+);
+
+
+
+
+router.post(
+  "/books",
+  asyncHandler(async (req, res) => {
+    const { search } = req.body;
+    const allBooks = await Book.findAll({
+      where: {
+        [Op.or]: [
+          {
+            title: {
+              [Op.like]: `%${search}%`,
+            },
+          },
+          {
+            author: {
+              [Op.like]: `%${search}%`,
+            },
+          },
+          {
+            genre: {
+              [Op.like]: `%${search}%`,
+            },
+          },
+          {
+            year: {
+              [Op.like]: `%${search}%`,
+            },
+          },
+        ],
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    
+    const pages = Math.ceil(allBooks.length / limit);
+    let page = 1;
+    console.log(req.query.page);
+    if (req.query.page) {
+      page = req.query.page;
+    }
+   
+    const books = allBooks.slice((page - 1) * limit, limit * page);
+    res.render("index", { books, pages, page, search });
+  })
+);
+
+router.get(
+  "/books/new",
+  asyncHandler(async (req, res) => {
+    res.render("new", { book: {} });
+  })
+);
+
+/* Posts a new book to the database */
+router.post(
+  "/books/new",
+  asyncHandler(async (req, res, next) => {
+    let book;
+    try {
+      book = await Book.create(req.body);
+      res.redirect("/books/");
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        book = await Book.build(req.body);
+        res.render("new", { book: book.dataValues, errors: error.errors });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
+
+router.get(
+  "/books/:id",
+  asyncHandler(async (req, res, next) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      res.render("update-book", { book });
+    } else {
+      const err = new Error();
+      err.status = 404;
+      res.render("page-not-found", { err });
+    }
+  })
+);
+
+router.post(
+  "/books/:id",
+  asyncHandler(async (req, res, next) => {
+    let book;
+    try {
+      book = await Book.findByPk(req.params.id);
+      if (book) {
+        await book.update(req.body);
+        res.redirect("/books/");
+      } else {
+        res.render("page-not-found");
+      }
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        book = await Book.build(req.body);
+        book.id = req.params.id;
+        res.render("update-book", { book, errors: error.errors });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
+
+router.post(
+  "/books/:id/delete",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.destroy();
+      res.redirect("/books");
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+module.exports = router;
